db8fcbafc8415664e6f7eb04f395ede7876f84df
ahelgeson <helgesonaustin@gmail.com>
2017-06-28 20:28:38 -0400

    Change list size to 6
    Changed list array to loop through 6 GPIO pins for "Google". Also
    updated comments at the top to appear more boilerplate like.

--- google_LED.py
+++ google_LED.py
@@ -1,46 +1,43 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 # google_LED.py
-
+# Python program to sequentially turn on 8 LEDs connected to GPIO 18, 23, 24, 25,
+# 12, 16, 17, and 27 individually at a 1/7 of a second per letter until the word
+# 'Google' is spelled, and then turn them off in reverse order at 1/4 of a second.
 # Import time and the RPi modules used in the script
+
 import time
 import RPi.GPIO as GPIO
-
-"""google_LED.py
-Python program to sequentially turn on 8 LEDs connected to GPIO 18, 23, 24, 25,
-12, 16, 17, and 27 individually at a 1/7 of a second per letter until the word
-'Google' is spelled, and then turn them off in reverse order at 1/4 of a second.
-"""
 
 # Define Variable 'RUNNING' as a boolean and set to 'True'.
 RUNNING = True
 
-# Define LED list and statically set the 8 GPIO pin numbers
-led_list = [18, 23, 24, 25, 12, 16, 17, 27]
+# Define LED list and statically set the 6 GPIO pin numbers
+led_list = [16, 12, 25, 24, 23, 18]
  
 # Set the GPIO to a BCM numbering scheme and set pins to output mode
 GPIO.setmode(GPIO.BCM)
-for x in range(0, 8):
+for x in range(0, 6):
     GPIO.setup(led_list[x], GPIO.OUT)
     GPIO.output(led_list[x], GPIO.LOW)
 
 print("Begin flashing Google sequence.")
 print("Press CTRL + C to quit.")
  
-# Main loop
+# Main sequence loop
 try:
     while RUNNING:
-        for x in range(0, 8):
+        for x in range(0, 6):
             GPIO.output(led_list[x], GPIO.HIGH)
             time.sleep(0.7)
-        for x in range(7, 0, -1):
+        for x in range(5, 0, -1):
             time.sleep(0.25)
             GPIO.output(led_list[x], GPIO.LOW)
  
 # CTRL+C will change variable 'RUNNING' to false and break the main loop.
 except KeyboardInterrupt:
     RUNNING = False
-    print "\Quitting"
+    print "\Stopping"
  
 # Actions under 'finally' will always be called
 # regardless of what stopped the program
